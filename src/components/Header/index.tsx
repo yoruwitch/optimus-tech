282e4554b1c70781f8130739094e43bd8b7d776b
yoruwitch <evelyn.fernandes4@gmail.com>
2024-12-09 10:47:56 -0300

    Feat: header section

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import "./style.css";
+function Header() {
+    return (
+        <>
+            <section className="header_container">
+                <span>Sobre nós</span>
+                <h1>Porque somos diferentes</h1>
+                <p>
+                    Nós focamos nos detalhes de tudo que fazemos. Tudo para
+                    ajudar as empresas de todo o mundo a se concentrarem naquilo
+                    que é realmente importante para elas.
+                </p>
+            </section>
+        </>
+    );
+}
+
+export default Header;
